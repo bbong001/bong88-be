2a20f4aab92074893c2864d9dba38997691fb28f
Dee <deeit789@gmail.com>
2024-10-18 13:59:26 +0700

    .

--- src/common/services/gs/gs.service.ts
+++ src/common/services/gs/gs.service.ts
@@ -3,6 +3,7 @@ import { HttpService } from '../http/http.service';
 import { ConfigService } from '@/config/config.service';
 import { CreatePlayer } from './interfaces/create-player.interface';
 import qs from 'qs';
+import { GetBalance } from './interfaces/get-balance.interface';
 
 @Injectable()
 export class GSService {
@@ -18,6 +19,19 @@ export class GSService {
   async createPlayer(_createPlayer: CreatePlayer): Promise<any> {
     const endpoint = `${this.gsApiUrl}/createMember.aspx`;
     const queryString = qs.stringify(_createPlayer);
+    const url = `${endpoint}?${queryString}`;
+
+    try {
+      const response = await this.httpService.get(url);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getBalance(_getBalance: GetBalance): Promise<any> {
+    const endpoint = `${this.gsApiUrl}/getBalance.aspx`;
+    const queryString = qs.stringify(_getBalance);
     const url = `${endpoint}?${queryString}`;
 
     try {
