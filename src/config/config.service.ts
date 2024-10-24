507b73063be4886e24bdca063094fe272745014a
Dee <deeit789@gmail.com>
2024-10-24 13:52:07 +0700

    update jwt using private key and public key

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -13,6 +13,16 @@ export class ConfigService {
     return this.configService.get<string>('JWT_SECRET_KEY');
   }
 
+  getJwtPublicKey(): string {
+    const _publicKey = this.configService.get<string>('JWT_PUBLIC_KEY');
+    return Buffer.from(_publicKey, 'base64').toString('utf-8');
+  }
+
+  getJwtPrivateKey(): string {
+    const _privateKey = this.configService.get<string>('JWT_PRIVATE_KEY');
+    return Buffer.from(_privateKey, 'base64').toString('utf-8');
+  }
+
   getJwtAccessTokenExpired(): string {
     return this.configService.get<string>('JWT_ACCESS_TOKEN_EXPIRED');
   }
